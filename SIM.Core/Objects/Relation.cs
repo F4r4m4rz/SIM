ccd6e71a3b6cfe9bb80442f633f2b1eb65d7922c
Faramarz Bodaghi <faramarz.bodaghi@aibel.com>
2020-05-16 16:57:26 +0200

    Generic Relation removed PropertyNode added

--- SIM.Core/Objects/Relation.cs
+++ SIM.Core/Objects/Relation.cs
@@ -14,16 +14,6 @@ namespace SIM.Core.Objects
         }
 
         /// <summary>
-        /// If the origin Node can have only one of this relation
-        /// </summary>
-        internal bool IsOriginSolo { get; set; }
-
-        /// <summary>
-        /// If the destination Node can accept only one of this relation
-        /// </summary>
-        internal bool IsDestinationSolo { get; set; }
-
-        /// <summary>
         /// The Node which lays at the start of the relation arrow
         /// </summary>
         public Node Origin { get; set; }
